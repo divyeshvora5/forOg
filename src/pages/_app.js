9f18d770297c244d97eaa68ed9792d9bdc9079dd
Divyesh <divyeshvora@logisticinfotech.co.in>
2024-06-14 16:03:19 +0530

    test

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -7,6 +7,8 @@ import { Guard, Layout } from "@/components";
 import { IntercomProvider } from "react-use-intercom";
 import { ThemeProvider } from "next-themes";
 import { useEffect } from "react";
+import Head from "next/head";
+import { FRONT_END_DOMAIN } from "@/constant";
 const WalletProvider = dynamic(() => import("../connection/WalletProvider"), {
 	ssr: false,
 });
@@ -16,37 +18,72 @@ const WalletProvider = dynamic(() => import("../connection/WalletProvider"), {
 // 	{ ssr: false }
 // );
 
-export default function App({ Component, pageProps }) {
+function App({ Component, pageProps }) {
+
+
+	//for og data
+	const ogData = pageProps?.ogData;
+
+
+
+
 	useEffect(() => {
 		if ("serviceWorker" in navigator) {
-		  navigator.serviceWorker
-			.register("/OneSignalSDKWorker.js")
-			.then((registration) => {
-			  console.log(
-				"Service Worker registered with scope:",
-				registration.scope
-			  );
-			})
-			.catch((error) => {
-			  console.error("Service Worker registration failed:", error);
-			});
+			navigator.serviceWorker
+				.register("/OneSignalSDKWorker.js")
+				.then((registration) => {
+					console.log(
+						"Service Worker registered with scope:",
+						registration.scope
+					);
+				})
+				.catch((error) => {
+					console.error("Service Worker registration failed:", error);
+				});
 		}
-	  }, []);
+	}, []);
 
 
 	return (
-		<WalletProvider >
-			    <ThemeProvider attribute="class">
-			<Provider store={store}>
-				<IntercomProvider appId="j5rgglth">
-					<Layout>
-						<Guard>
-							<Component {...pageProps} />
-						</Guard>
-					</Layout>
-				</IntercomProvider>
-			</Provider>
-			</ThemeProvider>
-		</WalletProvider>
+		<>
+			<Head>
+				<title>TesseractX</title>
+				<meta
+					name="description"
+					content="TesseractX is the ultimate NFT marketplace on PulseChain, offering generous rewards. Buy, sell, and trade unique digital collectibles!"
+				/>
+				<meta name="viewport" content="width=device-width, initial-scale=1" />
+				<meta property="og:type" content="website" />
+				<meta property="og:url" content={ogData?.url} />
+				<meta property="og:title" content="Your Title" />
+				<meta property="og:description" content="Your Description" />
+				<meta property="og:image" content={ogData?.imgUrl} />
+				<meta name="twitter:card" content="summary_large_image" />
+				<meta property="twitter:url" content={ogData?.url} />
+				<meta property="twitter:title" content="Your Title" />
+				<meta property="twitter:description" content="Your Description" />
+				<meta property="twitter:image" content={ogData?.imgUrl} />
+			</Head>
+			<WalletProvider >
+				<ThemeProvider attribute="class">
+					<Provider store={store}>
+						<IntercomProvider appId="j5rgglth">
+							<Layout>
+								<Guard>
+									<Component {...pageProps} />
+								</Guard>
+							</Layout>
+						</IntercomProvider>
+					</Provider>
+				</ThemeProvider>
+			</WalletProvider>
+		</>
 	);
 }
+
+App.getInitialProps = async (appContext) => {
+	const appProps = await App.getInitialProps(appContext);
+	return { ...appProps };
+};
+
+export default App;
--- src/pages/explore/collection/[itemCollection]/index.js
+++ src/pages/explore/collection/[itemCollection]/index.js
@@ -1,8 +1,25 @@
+import PageTitle from "@/components/Common/PageTitle";
 import { PublicProfile } from "@/components/PublicProfile/PublicProfileProvider";
 import CollectionDetailComponent from "@/components/collectionDetail/CollectionDetailComponent";
+import { FRONT_END_DOMAIN } from "@/constant";
+
+
+
+export async function getServerSideProps(context) {
+    // Fetch data for the specific page
+    const ogImageUrl = `${FRONT_END_DOMAIN}/api/og?title=${encodeURIComponent('Test')}&imageUrl=${encodeURIComponent('https://d17ha18jyelis7.cloudfront.net/collections/originals/16395541-68bd-4f66-a581-6528fb9ffbf4-1705050528582')}`;
+    return {
+      props: {
+        ogData: {
+            url: `${FRONT_END_DOMAIN}/explore/collection/0x49a708fd428319b543279826d9eadc9eab4ad888` ,
+            imgUrl: ogImageUrl
+        },
+      },
+    };
+  }
 import React from "react";
 
-const index = () => {
+const Index = ({ogData}) => {
     return (
         <>
             <PublicProfile>
@@ -12,4 +29,4 @@ const index = () => {
     );
 };
 
-export default index;
+export default Index;
